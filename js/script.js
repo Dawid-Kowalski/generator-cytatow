d8c542ab8992e70753e0a1fb5e6ccfe37dfbec40
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-20 13:04:50 +0100

    Add function to get and create tweet text

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+	fetch(quoteUrl, { cache: "no-store" })
+
+	.then(function(resp) {
+		return resp.json();
+	})
+
+	.then(createTweet);
+}
+
+function createTweet(input) {
+    let data = input[0];
+
+    let dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    let quoteText = dataElement.innerText.trim();
+    let quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    let tweetText = "Cytat dnia - " + quoteText + " Autor: " + quoteAuthor;
+
+    console.log(quoteText);
+    console.log(quoteAuthor);
+    console.log(tweetText);
+}
+
